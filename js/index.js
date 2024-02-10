5c0bfcd073cf0548b61deb6fa03235002febd376
Franco Pedreira <franco.pedreira2@gmail.com>
2024-02-10 03:00:28 -0300

    Se crea interacción con el usuario mediante prompt  para pedirle su ubicación de entrega de pedidos

--- js/index.js
+++ js/index.js
@@ -1,49 +1,3 @@
-// function sumarProductos(articulo) {
-//     let cantidad
-//     let montoFinal = 0
-//     let remera = 10000
-//     let buzo = 30000
-//     let jean = 25500
-//     let campera = 60000
-
-//     while (articulo != '*') {
-//         cantidad = prompt("Ingrese la cantidad de unidades para este producto");
-//         cantidad = parseInt(cantidad)
-
-//         if (articulo == "1") {
-//             // console.log("Se sumo el articulo remera al carrito (cantidad : " + cantidad + ")");
-//             console.log(`Se sumo el articulo remera al carrito (cantidad : ${cantidad})`);
-//             montoFinal += (remera * cantidad);
-//         }
-//         else if (articulo == "2"){
-//             console.log(`Se sumo el articulo buzo con estampado al carrito (cantidad : ${cantidad})`);
-//             montoFinal += (buzo * cantidad);
-//         }
-//         else if (articulo == "3"){
-//             // console.log("Se sumo el articulo jean roto holgado al carrito (cantidad : " + cantidad + ")");
-//             console.log(`Se sumo el articulo jean roto holgado al carrito (cantidad : ${cantidad})`);
-//             montoFinal += (jean * cantidad);
-//         }
-//         else if (articulo == "4"){
-//             // console.log("Se sumo el articulo campera inflada al carrito (cantidad : " + cantidad + ")");
-//             console.log(`Se sumo el articulo campera al carrito (cantidad : ${cantidad})`);
-//             montoFinal += (campera * cantidad);
-//         }
-//         articulo = prompt("Seleccione el tipo de producto que desea agregar para su compra: \n 1) Remera lisa - $10.000 \n 2) Buzo con estampado en la espalda - $30.000 \n 3) Jean roto holgado - 25.500 \n 4) Campera rompevientos - 60.000");
-//     }
-//     alert("El monto total de tu compra es de $" + montoFinal + " ARS");
-//     console.log("El monto total de tu compra es de $" + montoFinal + " ARS");
-// }
-
-// function main() {
-//     let articulo;
-
-//     articulo = prompt("Seleccione el tipo de producto que desea agregar para su compra: \n 1) Remera lisa - $10.000 \n 2) Buzo con estampado en la espalda - $30.000 \n 3) Jean roto holgado - 25.500 \n 4) Campera rompevientos - 60.000");
-//     sumarProductos(articulo);
-// }
-
-// main();
-
 // Podrias usar lo que vimos hace 2 clases: metodos como filter(); filtras el array por una categoria, ej: "remeras", y mostras en la seccion solo las remeras.
 
 // const SECCIONES = [
@@ -51,6 +5,27 @@
 //     {text: "Todos los productos", url: "../index.html"},
 //     {text: "Todos los productos", url: "../carrito.html"}
 // ]
+
+//Pido lo que necesito al HTML
+const GETDIRECTION = document.getElementById("direccion");
+const ALLPRODUCTS = document.getElementById("contenedor-productos");
+
+//Pido datos al usuario para mostrarlos en pantalla simulando por ejemplo que la ubicación ingresada es donde se van a entregar los productos
+//let nombreUsuario = prompt("Por favor, ingrese su nombre");
+let direccionUsuario = prompt("Por favor, ingrese la dirección en la que se entregaran los pedidos");
+
+if (direccionUsuario) {
+    direccionUsuario = direccionUsuario.charAt(0).toUpperCase() + direccionUsuario.slice(1); // Convertimos la primera letra a mayúscula y concatenamos el resto de la cadena
+}
+
+function agregarDireccon(direccionUsuario) {
+    const DIRECCION = document.createElement("div");
+    DIRECCION.classList.add("direccion");
+    DIRECCION.innerHTML = `
+                           <p><i class="bi bi-geo-alt"></i> Enviar a: ${direccionUsuario}</p></i>
+                          `
+    GETDIRECTION.appendChild(DIRECCION);
+}
 
 class Producto {
     constructor(id, nombre, imagen, precio) {
@@ -62,28 +37,25 @@ class Producto {
 }
 
 const productosArray = [
-    new Producto("1", "Campera Jean", "images/abrigos/01.jpg", 2000),
-    new Producto("2", "Buzo Crema", "images/abrigos/02.jpg", 2000),
-    new Producto("3", "Campera Impermeable", "images/abrigos/03.jpg", 2000),
-    new Producto("4", "Sobretodo Marron", "images/abrigos/SacoMujer.jpg", 2000),
-    new Producto("5", "Pantalon Skinny Fit", "images/pantalones/01.jpg", 2000),
-    new Producto("6", "Bermuda Jogger", "images/pantalones/02.jpg", 2000),
-    new Producto("7", "Pantalon Cinturon Combinado", "images/pantalones/03.jpg", 2000),
-    new Producto("8", "Pantalones Estructura", "images/pantalones/04.jpg", 2000),
-    new Producto("9", "Pantalones Cargo", "images/pantalones/05.jpg", 2000),
-    new Producto("10", "Remera Piqué", "images/remeras/01.jpg", 2000),
-    new Producto("11", "Remera Básica Azul", "images/remeras/02.jpg", 2000),
-    new Producto("12", "Chomba Violeta", "images/remeras/03.jpg", 2000),
-    new Producto("13", "Remera Estampado Combinado", "images/remeras/04.jpg", 2000),
-    new Producto("14", "Remera Punto", "images/remeras/05.jpg", 2000)
+    new Producto("1", "Campera Jean", "/images/abrigos/01.jpg", 2000),
+    new Producto("2", "Buzo Crema", "/images/abrigos/02.jpg", 2000),
+    new Producto("3", "Campera Impermeable", "/images/abrigos/03.jpg", 2000),
+    new Producto("4", "Sobretodo Marron", "/images/abrigos/SacoMujer.jpg", 2000),
+    new Producto("5", "Pantalon Skinny Fit", "/images/pantalones/01.jpg", 2000),
+    new Producto("6", "Bermuda Jogger", "/images/pantalones/02.jpg", 2000),
+    new Producto("7", "Pantalon Cinturon Combinado", "/images/pantalones/03.jpg", 2000),
+    new Producto("8", "Pantalones Estructura", "/images/pantalones/04.jpg", 2000),
+    new Producto("9", "Pantalones Cargo", "/images/pantalones/05.jpg", 2000),
+    new Producto("10", "Remera Piqué", "/images/remeras/01.jpg", 2000),
+    new Producto("11", "Remera Básica Azul", "/images/remeras/02.jpg", 2000),
+    new Producto("12", "Chomba Violeta", "/images/remeras/03.jpg", 2000),
+    new Producto("13", "Remera Estampado Combinado", "/images/remeras/04.jpg", 2000),
+    new Producto("14", "Remera Punto", "/images/remeras/05.jpg", 2000)
 ]
-
-const ALLPRODUCTS = document.getElementById("contenedor-productos");
 
 function agregarProductos(productos) {
     productos.forEach(producto => {
         const CARD = document.createElement("div");
-        CARD.classList.add("card");
         CARD.innerHTML = `
                         <div class="producto">
                             <img class="producto-imagen" src="${producto.imagen}" alt="">
@@ -98,4 +70,9 @@ function agregarProductos(productos) {
     });
 }
 
-agregarProductos(productosArray);
+function main() {
+    agregarProductos(productosArray);
+    agregarDireccon(direccionUsuario);
+}
+
+main();
